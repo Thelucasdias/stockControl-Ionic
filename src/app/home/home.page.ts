fe8354694e600d6cc086127a1effa5fc7149733b
Thelucasdias <llucasdias@yahoo.com.br>
2024-12-16 17:34:26 -0300

    loginScreen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    console.log('largura:', window.innerWidth)
+    console.log('altura:', window.innerHeight)
+  }
 }
